e99b657d803c4e60e0b75813f776b9629324b046
Fabian Altahona <fabian.altahona@koombea.com>
2020-06-30 09:01:39 -0500

    Feed: Refactor main Button Feed: Add extra CSS Feed: Add font Archivo Black

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 /* enqueue scripts and style from parent theme */
 function twentytwenty_styles() {
-	wp_enqueue_style( 'parent', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'parent', get_stylesheet_directory_uri() . '/dist/styles/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'twentytwenty_styles');
+
